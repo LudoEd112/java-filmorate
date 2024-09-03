5418a2e3906c7cdc9a1e3d9ac8492e151beae2de
LudoEd <datakmakov112@yandex.ru>
2024-09-04 00:57:57 +0300

    Спринт 10, выполнено ТЗ, комит №1

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
-import java.time.Month;
 
 import static org.junit.jupiter.api.Assertions.*;
 
