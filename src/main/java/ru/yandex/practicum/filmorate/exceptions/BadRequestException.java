ab6b4ee6d589e6d0d752ba25c9c331133dee2644
datak <datakmakov@gmail.com>
2024-10-17 05:27:17 +0300

    Спринт 12, выполнено задание

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/BadRequestException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/BadRequestException.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message){
+    public BadRequestException(String message) {
         super(message);
     }
 }
