a9d6758dec5617e66e6f370dd255ab2d76ddfae0
datak <datakmakov@gmail.com>
2024-10-17 05:29:29 +0300

    Спринт 12, выполнено задание

--- src/main/java/ru/yandex/practicum/filmorate/handler/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ErrorHandler.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,7 +12,6 @@ import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -35,28 +33,6 @@ public class ErrorHandler {
                 e.getMessage()
         );
     }
-
-    /*@ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleThrowable(final Throwable e) {
-        log.error("handleThrowable: {}", e.getMessage());
-        return new ErrorResponse(
-                e.getMessage()
-        );
-    }*/
-
-    /*@ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
-        log.error("handleMethodArgumentNotValidException: {}", e.getMessage());
-        return new ErrorResponse(
-                e.getBindingResult()
-                        .getFieldErrors()
-                        .stream()
-                        .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
-                        .collect(Collectors.joining("; "))
-        );
-    }*/
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
